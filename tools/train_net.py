f80eb900e12d7da088bfe0aa0bb09d2db1d357e3
ChrisLee63 <zjli1997@163.com>
2020-04-27 21:13:28 +0800

    test

--- tools/train_net.py
+++ tools/train_net.py
@@ -1,14 +1,8 @@
 import detectron2.utils.comm as comm
 from detectron2.checkpoint import DetectionCheckpointer
 from detectron2.config import get_cfg
-from detectron2.engine import (
-    DefaultTrainer,
-    default_argument_parser,
-    default_setup,
-    launch,
-    verify_results,
-)
-from detectron2.evaluation import DatasetEvaluators
+from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, launch
+from detectron2.evaluation import DatasetEvaluators, verify_results
 
 from src import CUHK_SYSU_Evaluator
 from src.cuhk_sysu import CUHK_SYSU
