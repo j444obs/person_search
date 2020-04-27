dd8059cfa5aa3d5248d1f1bff80bb834f18c5dac
ChrisLee63 <zjli1997@163.com>
2020-04-27 19:00:03 +0800

    test

--- tools/train_net.py
+++ tools/train_net.py
@@ -1,5 +1,13 @@
+import detectron2.utils.comm as comm
+from detectron2.checkpoint import DetectionCheckpointer
 from detectron2.config import get_cfg
-from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, launch
+from detectron2.engine import (
+    DefaultTrainer,
+    default_argument_parser,
+    default_setup,
+    launch,
+    verify_results,
+)
 from detectron2.evaluation import DatasetEvaluators
 
 from src import CUHK_SYSU_Evaluator
@@ -26,15 +34,15 @@ def setup(args):
 def main(args):
     cfg = setup(args)
 
-    # if args.eval_only:
-    #     model = Trainer.build_model(cfg)
-    #     DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
-    #         cfg.MODEL.WEIGHTS, resume=args.resume
-    #     )
-    #     res = Trainer.test(cfg, model)
-    #     if comm.is_main_process():
-    #         verify_results(cfg, res)
-    #     return res
+    if args.eval_only:
+        model = Trainer.build_model(cfg)
+        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+            cfg.MODEL.WEIGHTS, resume=args.resume
+        )
+        res = Trainer.test(cfg, model)
+        if comm.is_main_process():
+            verify_results(cfg, res)
+        return res
 
     trainer = Trainer(cfg)
     trainer.resume_or_load(resume=args.resume)
