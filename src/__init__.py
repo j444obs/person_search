844be458675c7b756254a6663f5b3cc4a1213ef4
ChrisLee63 <zjli1997@163.com>
2020-04-27 11:54:34 +0800

    Implementation based on detectron2

---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+from . import cuhk_sysu
--- src/cuhk_sysu.py
+++ src/cuhk_sysu.py
@@ -1,75 +1,66 @@
 import logging
-import os
 import os.path as osp
 
 import numpy as np
 from PIL import Image
 from scipy.io import loadmat
-from torch.utils.data import Dataset
 
-from datasets.data_processing import build_net_input
-from utils.config import cfg
-from utils.utils import pickle, unpickle
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.structures import BoxMode
 
 
-class PSDB(Dataset):
-    """Person search database."""
-
-    def __init__(self, db_name, root_dir=None):
+class CUHK_SYSU:
+    def __init__(self, root_dir, db_name):
         self.db_name = db_name
-        self.root_dir = root_dir if root_dir else osp.join(cfg.DATA_DIR, "dataset")
+        self.root_dir = root_dir
         self.data_path = osp.join(self.root_dir, "Image", "SSM")
         self.classes = ["background", "person"]
         self.image_index = self.load_image_index()
         self.roidb = self.load_roidb()
-        if db_name == "psdb_test":
+        if db_name == "test":
             self.probes = self.load_probes()
-        if db_name == "psdb_train" and cfg.TRAIN.USE_FLIPPED:
-            self.append_flipped_images()
+        # if db_name == "train":
+        #     self.append_flipped_images()
 
     @property
     def num_images(self):
         return len(self.image_index)
-
-    def __len__(self):
-        return len(self.roidb)
-
-    def __getitem__(self, index):
-        return build_net_input(self.roidb[index])
 
     def image_path_at(self, i):
         image_path = osp.join(self.data_path, self.image_index[i])
         assert osp.isfile(image_path), "Path does not exist: %s" % image_path
         return image_path
 
-    def append_flipped_images(self):
-        num_images = len(self.image_index)
-        widths = [Image.open(self.image_path_at(i)).size[0] for i in range(num_images)]
-        for i in range(num_images):
-            gt_boxes = self.roidb[i]["gt_boxes"].copy()
-            oldx1 = gt_boxes[:, 0].copy()
-            oldx2 = gt_boxes[:, 2].copy()
-            gt_boxes[:, 0] = widths[i] - oldx2 - 1
-            gt_boxes[:, 2] = widths[i] - oldx1 - 1
-            assert (gt_boxes[:, 2] >= gt_boxes[:, 0]).all()
-            entry = {
-                "gt_boxes": gt_boxes,
-                "gt_pids": self.roidb[i]["gt_pids"],
-                "image": self.roidb[i]["image"],
-                "height": self.roidb[i]["height"],
-                "width": self.roidb[i]["width"],
-                "flipped": True,
-            }
-            self.roidb.append(entry)
-        self.image_index = self.image_index * 2
+    # def append_flipped_images(self):
+    #     num_images = len(self.image_index)
+    #     widths = [Image.open(self.image_path_at(i)).size[0] for i in range(num_images)]
+    #     for i in range(num_images):
+    #         gt_boxes = self.roidb[i]["gt_boxes"].copy()
+    #         oldx1 = gt_boxes[:, 0].copy()
+    #         oldx2 = gt_boxes[:, 2].copy()
+    #         gt_boxes[:, 0] = widths[i] - oldx2 - 1
+    #         gt_boxes[:, 2] = widths[i] - oldx1 - 1
+    #         assert (gt_boxes[:, 2] >= gt_boxes[:, 0]).all()
+    #         entry = {
+    #             "gt_boxes": gt_boxes,
+    #             "gt_pids": self.roidb[i]["gt_pids"],
+    #             "image": self.roidb[i]["image"],
+    #             "height": self.roidb[i]["height"],
+    #             "width": self.roidb[i]["width"],
+    #             "flipped": True,
+    #         }
+    #         self.roidb.append(entry)
+    #     self.image_index = self.image_index * 2
 
     def load_image_index(self):
-        """Load the image indexes for training / testing."""
+        """
+        Load the image indexes for training / testing.
+        """
         # Test images
         test = loadmat(osp.join(self.root_dir, "annotation", "pool.mat"))
         test = test["pool"].squeeze()
         test = [str(a[0]) for a in test]
-        if self.db_name == "psdb_test":
+        if self.db_name == "test":
             return test
 
         # All images
@@ -83,7 +74,9 @@ class PSDB(Dataset):
         return train
 
     def load_probes(self):
-        """Load the list of (img, roi) for probes."""
+        """
+        Load the list of (img, roi) for probes.
+        """
         protocol = loadmat(osp.join(self.root_dir, "annotation/test/train_test/TestG50.mat"))
         protocol = protocol["TestG50"].squeeze()
         probes = []
@@ -106,12 +99,12 @@ class PSDB(Dataset):
             height: int, image height
             flipped: bool, whether the image is horizontally-flipped
         """
-        cache_path = osp.join(cfg.DATA_DIR, "cache")
-        if not osp.exists(cache_path):
-            os.makedirs(cache_path)
-        cache_file = osp.join(cache_path, self.db_name + "_roidb.pkl")
-        if osp.isfile(cache_file):
-            return unpickle(cache_file)
+        # cache_path = osp.join(cfg.DATA_DIR, "cache")
+        # if not osp.exists(cache_path):
+        #     os.makedirs(cache_path)
+        # cache_file = osp.join(cache_path, self.db_name + "_roidb.pkl")
+        # if osp.isfile(cache_file):
+        #     return unpickle(cache_file)
 
         # Load all images and build a dict from image to boxes
         all_imgs = loadmat(osp.join(self.root_dir, "annotation", "Images.mat"))
@@ -137,7 +130,7 @@ class PSDB(Dataset):
 
         # Load all the train / test persons and label their pids from 0 to N - 1
         # Assign pid = -1 for unlabeled background people
-        if self.db_name == "psdb_train":
+        if self.db_name == "train":
             train = loadmat(osp.join(self.root_dir, "annotation/test/train_test/Train.mat"))
             train = train["Train"].squeeze()
             for index, item in enumerate(train):
@@ -179,9 +172,43 @@ class PSDB(Dataset):
                     "image": self.image_path_at(i),
                     "height": size[1],
                     "width": size[0],
-                    "flipped": False,
+                    # "flipped": False,
                 }
             )
-        pickle(roidb, cache_file)
-        logging.info("Save ground-truth roidb to: %s" % cache_file)
+        # pickle(roidb, cache_file)
+        # logging.info("Save ground-truth roidb to: %s" % cache_file)
         return roidb
+
+
+def load_cuhk_sysu_instances(dirname, split):
+    dataset = CUHK_SYSU(dirname, split)
+    dicts = []
+    for i, image in enumerate(dataset.image_index):
+        dict = {}
+        dict["file_name"] = dataset.roidb[i]["image"]
+        dict["image_id"] = image
+        dict["height"] = dataset.roidb[i]["height"]
+        dict["width"] = dataset.roidb[i]["width"]
+        # dict["flipped"] = dataset.roidb[i]["flipped"]
+        instances = []
+        for j, gt_box in enumerate(dataset.roidb[i]["gt_boxes"]):
+            instances.append(
+                {
+                    "bbox": gt_box,
+                    "bbox_mode": BoxMode.XYXY_ABS,
+                    "category_id": 1,
+                    "person_id": dataset.roidb[i]["gt_pids"][j],
+                }
+            )
+        dict["annotations"] = instances
+        dicts.append(dict)
+    return dicts
+
+
+def register_cuhk_sysu(dirname):
+    DatasetCatalog.register("cuhk_sysu_train", lambda: load_cuhk_sysu_instances(dirname, "train"))
+    DatasetCatalog.register("cuhk_sysu_test", lambda: load_cuhk_sysu_instances(dirname, "test"))
+    MetadataCatalog.get("cuhk_sysu").set(thing_classes=["background", "person"], dirname=dirname)
+
+
+register_cuhk_sysu()
